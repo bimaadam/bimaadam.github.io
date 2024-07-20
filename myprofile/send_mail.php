9d3147bf20504bc31e34d46be0d349bf3bd717cc
bimaadam <bimavyanisty12@gmail.com>
2024-07-20 16:17:02 +0700

    fix bug on contact

---
+++ myprofile/send_mail.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "bimaadamrailfans35@gmail.com"; 
+    $subject = "New Contact Form Submission";
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again later.";
+    }
+}
+?>
