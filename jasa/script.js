7e8c3fbeaa56b3e23ed875403794a07a75d8dc44
rinbim <rinbim.dev@gmail.com>
2025-05-12 14:27:56 +0700

    fix styles

--- jasa/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const ctaButton = document.querySelector('.cta-button');
-    ctaButton.addEventListener('click', function() {
-        alert('Terima kasih telah mengunjungi Rinbim.dev!');
-    });
-});
