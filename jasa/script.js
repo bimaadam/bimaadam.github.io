34b54f03594f8a4dd49ac0e48d42d33c0fd5ff88
rinbim <rinbim.dev@gmail.com>
2025-05-12 14:07:16 +0700

    add page jasa

---
+++ jasa/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', function() {
+        alert('Terima kasih telah mengunjungi Rinbim.dev!');
+    });
+});
